35aa81cdc2f1f0b72ad7ecd5424133cfbf7923a0
Surendiran S <surenyas1902@gmail.com>
2021-10-23 19:40:09 +0530

    Initial commit

---
+++ src/main/java/com/surendiran/crudmvc/CrudMvcForSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.surendiran.crudmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMvcForSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMvcForSpringBootApplication.class, args);
+	}
+
+}
